6ac94f1f6d3301308222f56fdc3d6d5e3e87bb64
Pavel Karoukin <pavel@karoukin.us>
2015-06-13 14:42:37 -0500

    Update compiled version

--- dist/index.js
+++ dist/index.js
@@ -21,18 +21,46 @@ var MapTable = function MapTable(rules) {
         throw new Error('rules are required for MapTable.');
     }
 
-    this.rules = rules;
-    this.init();
+    this.rules = [];
+    this.cols = [];
+
+    this.init(rules);
 };
 
-MapTable.prototype.init = function() {
+MapTable.prototype.init = function(rules) {
+    rules = _.clone(rules);
+
     this.match = _.memoize(MapTable.prototype.match);
+
+    this.cols = rules.shift();
+
+    if (! _.isArray(this.cols)) {
+        throw new Error('First row of rules array should be array of columns.');
+    }
+
+    this.rules = rules;
+};
+
+MapTable.prototype.rowToObject = function(row) {
+    if (row == null) {
+        return null;
+    }
+
+    var obj = {};
+
+    this.cols.forEach(function(col, idx) {
+        obj[col] = row[idx];
+    });
+
+    return obj;
 };
 
 MapTable.prototype.match = function(values) {
     if (!_.isObject(values)) {
         throw new Error('values should be object');
     }
+
+    var that = this;
 
     var matchedRule = null;
     var valueKeys = _.keys(values);
@@ -41,7 +69,15 @@ MapTable.prototype.match = function(values) {
         var match = true;
 
         valueKeys.some(function(key) {
-            if (!rule[key] || rule[key] !== values[key]) {
+            var idx = that.cols.indexOf(key);
+
+            // If key is not found in the rules table, assume it is not applicable,
+            // and just move on without invalidating match.
+            if (idx < 0) {
+                return false;
+            }
+
+            if (!rule[idx] || rule[idx] !== values[key]) {
                 match = false;
                 return true;
             }
@@ -53,7 +89,7 @@ MapTable.prototype.match = function(values) {
         }
     });
 
-    return matchedRule;
+    return this.rowToObject(matchedRule);
 };
 
 if (module && module.exports) {
